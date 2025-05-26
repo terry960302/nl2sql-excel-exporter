301ff17b00cd09d0de380bb538ee1f6e85aba57e
Taewan Kim <terry960302@gmail.com>
2025-05-26 16:49:09 +0900

    :sparkles: new schema service

---
+++ apps/schema-microservice/src/main/java/com/pandaterry/schema_microservice/SchemaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pandaterry.schema_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemaMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchemaMicroserviceApplication.class, args);
+	}
+
+}
---
+++ apps/schema-microservice/src/test/java/com/pandaterry/schema_microservice/SchemaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandaterry.schema_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
