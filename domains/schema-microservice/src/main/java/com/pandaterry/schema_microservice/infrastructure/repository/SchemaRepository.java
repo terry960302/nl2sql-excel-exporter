99dc082eeaec18e7f0cf0752ff8586864c7f4310
Taewan Kim <37768791+terry960302@users.noreply.github.com>
2025-06-05 23:00:24 +0900

    :recycle: Schema isEnabled 필드 삭제 및 Repository 수정

--- domains/schema-microservice/src/main/java/com/pandaterry/schema_microservice/infrastructure/repository/SchemaRepository.java
+++ domains/schema-microservice/src/main/java/com/pandaterry/schema_microservice/infrastructure/repository/SchemaRepository.java
@@ -1,7 +1,6 @@
 package com.pandaterry.schema_microservice.infrastructure.repository;
 
 import com.pandaterry.schema_microservice.domain.entity.Schema;
-import com.pandaterry.msa_contracts.enums.schema.EnableStatus;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface SchemaRepository extends JpaRepository<Schema, UUID> {
-    List<Schema> findByDatasourceIdAndIsEnabled(UUID datasourceId, EnableStatus isEnabled);
+    List<Schema> findByDatasourceId(UUID datasourceId);
 }
