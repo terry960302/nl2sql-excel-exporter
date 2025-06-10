5cb3a1e812ad707a57e3dc15114ee0548195a990
Taewan Kim <terry960302@gmail.com>
2025-06-10 15:46:33 +0900

    :rectcle: gateway 인증 서버 연결 설정

--- backend/domains/auth-microservice/src/main/java/com/pandaterry/auth_microservice/presentation/controller/AuthController.java
+++ backend/domains/auth-microservice/src/main/java/com/pandaterry/auth_microservice/presentation/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1" + RoutePath.Auth.BASE)
+@RequestMapping(RoutePath.Auth.BASE)
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
